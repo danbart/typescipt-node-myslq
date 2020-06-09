96bfc0f4c4e4c931885257788d2f90274f3feb4e
danbart <danilo_solorzano06@hotmail.com>
2020-06-08 18:46:20 -0600

    index

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const server = Server.init( 3000 );
 server.app.use( router );
 
 // MySql Instance
-// MySql.instances;
+MySql.instances;
 
 server.start(() => {
     console.log("Servidor corriendo en el puerto 3000");
